1d34f611fefa5e6c52c7d9b54c59b8e03c13aab8
Prince <princetonjudecipriano@gmail.com>
2023-10-07 17:55:31 +0800

    initial commit

---
+++ src/data/user.js
@@ -0,0 +1,94 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Princeton Jude Liban Cipriano",
+		email: "princetonjudecipriano@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/CiprianoPrince",
+		linkedin:
+			"https://www.linkedin.com/in/princeton-jude-cipriano-619860224/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://web.facebook.com/princecipriano25/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer.",
+		description:
+			"I am an up-and-coming web developer with a versatile skill set, encompassing both frontend and backend development, with a particular expertise in Node.js. I am driven by the challenge of creating scalable, secure, and dependable web applications using a variety of frameworks and technologies. The thrill of solving complex problems and the excitement of continuous learning fuel my passion for web development. I am deeply committed to producing top-notch code that adheres to best practices and industry standards. My journey is just beginning, and I am eagerly seeking new opportunities and challenges to grow and excel in both frontend and backend development.",
+	},
+
+	about: {
+		title: "I’m Princeton Jude Cipriano I live in Philippines, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Abdevify's Official Website",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/CiprianoPrince/ABDevify",
+		},
+
+		{
+			title: "Bgm Philippines",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/CiprianoPrince/bgm-philippines",
+		},
+
+		{
+			title: "O-kayto",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/CiprianoPrince/okayto-client",
+		},
+
+		{
+			title: "Print State",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/CiprianoPrince/print-state",
+		},
+
+		{
+			title: "Real State",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/CiprianoPrince/real-estate",
+		},
+
+		{
+			title: "Tic Tac Toe",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/CiprianoPrince/tic-tac-toe-react",
+		},
+	],
+};
+
+export default INFO;
